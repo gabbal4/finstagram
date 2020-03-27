99b7d809ba3776cdc01d1ae57f5ab3a69fb99a5c
gabbal4 <gabbalfour@gmail.com>
2020-03-27 00:50:09 +0000

    w5d2

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 class FinstagramPost < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
+  has_many :likes
 
-    belongs_to :user
-    has_many :comments
-    has_many :likes
+  validates :photo_url, :user, presence: true
 
-    validates_presence_of :user
+  def humanized_time_ago
+    time_ago_in_seconds = Time.now - created_at
+    time_ago_in_minutes = time_ago_in_seconds / 60
 
-    def humanized_time_ago
-        time_ago_in_seconds = Time.now - self.created_at
-        time_ago_in_minutes = time_ago_in_seconds / 60
-
-        if time_ago_in_minutes >= 60
-            "#{(time_ago_in_minutes / 60).to_i} hours ago"
-        else
-            "#{time_ago_in_minutes.to_i} minutes ago"
-        end
+    if time_ago_in_minutes >= 60
+      "#{(time_ago_in_minutes / 60).to_i} hours ago"
+    else
+      "#{time_ago_in_minutes.to_i} minutes ago"
     end
+  end
 
-    def like_count
-        self.likes.size
-    end
-    
-    def comment_count
-        self.comments.size
-    end
+  def like_count
+    likes.size
+  end
+
+  def comment_count
+    comments.size
+  end
 end
